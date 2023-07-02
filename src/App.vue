d88cc559598b044a0d4883a9a7fa8f9745761bf1
Sam Norris-Rogers <sam.norris-rogers@genexinstitute.com>
2023-07-02 21:06:43 +0200

    set up project

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+import Random from './components/Random.vue'
+import { onMounted, ref } from 'vue'
+
+const fact = ref('')
+
+const getFact = async () => {
+  const response = await fetch('https://uselessfacts.jsph.pl/random.json?language=en')
+  const data = await response.json()
+  fact.value = data.text
+}
+
+onMounted(() => {
+  getFact()
+})
+
+</script>
+
+<template>
+  <main>
+    <h3>Here's your Random Fact of the Day</h3>
+    {{ fact }}
+  </main>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Random.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+</script>
+<template>
+    <main>
+        {{ fact.value }}
+        <h1>Here's your Random Fact of the Day</h1>
+    </main>
+</template>
+<style scoped></style>
